3367098b6c53056c994f8f395ad02d73f3eae3b9
Nice_X <777soer@gmail.com>
2023-11-13 22:25:45 +0300

    react commit

---
+++ src/Message.js
@@ -0,0 +1,11 @@
+function Message(props) {
+    return (
+        <div className="text">
+            Hello, i'm {props.name}, i'm 20 year old 
+        </div>
+    )
+}
+
+
+
+export default Message
